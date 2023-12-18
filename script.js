f6fe6fb79a5e73f720ef971a35f5fbd622b32b13
jasmeen1234 <jasmeenbano12@gmail.com>
2023-12-18 22:36:34 +0530

    j

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Initial box generation
+    generateBoxes(1);
+  });
+  
+  function generateBoxes(num) {
+    const boxContainer = document.getElementById('boxContainer');
+    boxContainer.innerHTML = ''; // Clear existing boxes
+  
+    for (let i = 0; i < num; i++) {
+      const box = document.createElement('div');
+      box.className = 'box';
+      box.style.bottom = i * (80 / num) + 'vh';
+      box.style.left = i * (80 / num) + 'vw';
+      box.style.width = 80/ num + 'vw';
+      box.style.height = 80 / num + 'vh';
+  
+      boxContainer.appendChild(box);
+    }
+  }
+  
+  function handleGenerateClick() {
+    const boxNumber = parseInt(document.getElementById('boxNumber').value);
+    generateBoxes(boxNumber);
+  }
+  
